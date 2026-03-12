ac9754360740129d1e9d5cd9c26b42117751e52f
PereAPrats <ppratsv03@gmail.com>
2026-03-12 20:17:18 +0100

    Refactor: agregar archivos de encabezado y pie de página, actualizar rutas de carga de componentes

--- public/js/main.js
+++ public/js/main.js
@@ -13,15 +13,15 @@ function cargarComponente(id, archivo) {
 document.addEventListener("DOMContentLoaded", () => {
     let ruta;
     const path = window.location.pathname;
-    // index.html (o raíz) ahora usa la carpeta pages/partials
+    // index.html (o raíz) ahora usa la carpeta html/partials
     if (path === '/' || path === '/index.html') {
-        ruta = 'pages/partials/';
-    } else if (path.includes('/pages/')) {
-        // una página dentro de /pages
+        ruta = 'html/partials/';
+    } else if (path.includes('/html/')) {
+        // una página dentro de /html
         ruta = 'partials/';
     } else {
         // caso genérico, por seguridad
-        ruta = 'pages/partials/';
+        ruta = 'html/partials/';
     }
     cargarComponente('header-placeholder', ruta + 'header.html');
     cargarComponente('footer-placeholder', ruta + 'footer.html');
